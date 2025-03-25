9f034dc3292e209b49e674ded5bbad8e96d547cf
Weezow <leonardovale97@gmail.com>
2025-03-25 18:45:51 +0000

    My first commit

---
+++ first.py
@@ -0,0 +1,14 @@
+def yourdad(val):
+    print(__name__)
+    a = val
+    b = val
+    if a == 1 :
+        print(a)
+    elif a == 2 : print("ya moms nicca")
+    else : print("error!!!")
+
+def callyourdad(var = 2):
+    yourdad(val = var)
+    print(var)
+
+callyourdad(1)
