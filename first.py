38d3ea631e5ab89fce94613e33ce8f111633ee2f
BlightedIris <blightediris@gmail.com>
2025-03-25 19:39:41 +0000

    added new list and dict demo

--- first.py
+++ first.py
@@ -12,3 +12,29 @@ def callyourdad(var = 2):
     print(var)
 
 callyourdad(1)
+
+l = [1, 2, 3]
+
+t = tuple(1,2,3)
+
+users = []
+
+def user(username, password, email):
+    return {
+        "username" : username,
+        "password" : password,
+        "email" : email
+    }
+
+users.append(user("Ricardo", "abcd", "yo@gmail.com"))
+users.append(user("Leo", "Weezow", "leo@gmail.com"))
+users.append(user("test", "test", "leo@gmail.com"))
+users.append(user("bruh", "bruh", "leo@gmail.com"))
+users.append(user("Sike", "sike", "leo@gmail.com"))
+
+print(users)    
+
+for usr in 10:
+    print(usr)
+    if usr["username"] == "Leo":
+        print(usr["email"])
